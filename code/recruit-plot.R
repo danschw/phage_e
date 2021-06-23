d3e73209c537e9c1cf281a39f521d1cc700f63dc
danschw <danschw@iu.edu>
2021-06-22 22:49:22 -0400

    prelim recruit plot

--- code/recruit-plot.R
+++ code/recruit-plot.R
@@ -15,7 +15,20 @@ readBlast <- function(hits)
   return (blast)
 }
 
+#### list sra accesion by study#####
+studies <- list.files(here("data/recruitment/sra_acc/"), 
+                pattern = ".txt", full.names = TRUE)
+d.studies <- tibble()
 
+for (f in studies){
+  d.studies <- read_tsv(f, col_names = F) %>% 
+    rename(acc=X1) %>% 
+    mutate(study = str_remove(f, ".txt")) %>% 
+    mutate(study = str_remove(study, ".*sra_acc/")) %>% 
+    bind_rows(d.studies,.)
+}
+
+#### Collect blast results#####
 blast_hits <- list.files(here("data/recruitment/tblastn/"), 
                          pattern = ".tsv", full.names = TRUE)
 
@@ -26,18 +39,29 @@ for (f in blast_hits){
     bind_rows(d,.)
 }
 
-d %>% 
-  ggplot(aes(alnLength, percIdentity)) +
-  geom_point()
+# add study
+d <- d %>% 
+  separate(subjectId, into = c("acc", "read", "pair"), sep = "\\.", remove = F) %>% 
+  left_join(., d.studies, by = "acc" )
 
+# # make numerical gene column
+# d <- d %>% 
+  
+# d %>% 
+#   ggplot(aes(alnLength, percIdentity)) +
+#   geom_point()
 
-x <- list.files(here("data/recruitment/sra_acc/"), 
-                pattern = ".txt", full.names = TRUE)
-d.acc <- tibble()
+p <- d %>% 
+  # slice_head(n = 10000) %>% 
+  ggplot(aes(parse_number(queryId), percIdentity)) +
+  geom_jitter(shape=21, alpha = 0.5)+
+  facet_grid(study ~ .)+
+  theme_classic()+
+  panel_border(color = "black", size = 1)+
+  xlab("phage E gene number")+
+  ylab("%ID (tblastn)")+
+  scale_x_continuous(breaks = seq(0,72,2))
+  # theme(axis.text.x = element_text(angle = 90))
 
-for (f in x){
-   d.acc <- read_tsv(f, col_names = F) %>% 
-    mutate(study = str_remove(f, ".txt")) %>% 
-     mutate(study = str_remove(study, ".*sra_acc/")) %>% 
-     bind_rows(d.acc,.)
-}
+ggsave(here("plots/recruit-prelim.png"), plot = p, width = 11, height = 8)  
+
